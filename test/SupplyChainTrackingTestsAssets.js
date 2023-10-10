4867b482d99e20cf90277f00f67da2e83f2f35ff
raphaelpg <raphael_pinto@outlook.com>
2023-10-10 16:50:23 +0200

    split tests into different test files

--- test/SupplyChainTrackingTestsAssets.js
+++ test/SupplyChainTrackingTestsAssets.js
@@ -15,73 +15,6 @@ describe("SupplyChainTracking tests", function () {
     return { owner, addr1, addr2, supplyChainTracking };
   }
 
-  // Test deployment
-  describe("Test deployment", async function () {
-    it("Should deploy SupplyChainTracking", async function () {
-      const { supplyChainTracking, owner } = await loadFixture(deploySupplyChainTrackingFixture);
-
-      expect(await supplyChainTracking.owner()).to.equal(owner.address);
-    });
-  });
-
-  // Test Actors functions
-  describe("Test Actors functions", async function () {
-    it("Should add an actor", async function () {
-      const { supplyChainTracking, addr1 } = await loadFixture(deploySupplyChainTrackingFixture);
-
-      const result = await supplyChainTracking.registerActor(addr1.address, 1);
-      expect(result).to.emit(supplyChainTracking, 'ActorRegistered');
-    })
-    
-    it("Should disable an actor", async function () {
-      const { supplyChainTracking, addr1 } = await loadFixture(deploySupplyChainTrackingFixture);
-
-      await supplyChainTracking.registerActor(addr1.address, 1);
-      expect(await supplyChainTracking.disableActor(addr1.address)).to.emit(supplyChainTracking, 'ActorDisabled');
-    })
-    
-    it("Should not allow unauthorized address to register an actor", async function () {
-      const { supplyChainTracking, addr1 } = await loadFixture(deploySupplyChainTrackingFixture);
-
-      const unauthorizedRegistration = supplyChainTracking.connect(addr1).registerActor(addr1.address, 1);
-      await expect(unauthorizedRegistration).to.be.revertedWithCustomError(supplyChainTracking, "OwnableUnauthorizedAccount");
-    })
-
-    it("Should not allow unauthorized address to disable an actor", async function () {
-      const { supplyChainTracking, addr1, addr2 } = await loadFixture(deploySupplyChainTrackingFixture);
-      
-      await supplyChainTracking.registerActor(addr1.address, 1);
-      const unauthorizedDisabling = supplyChainTracking.connect(addr2).disableActor(addr1.address);
-      await expect(unauthorizedDisabling).to.be.revertedWithCustomError(supplyChainTracking, "OwnableUnauthorizedAccount");
-    })
-    
-    it("Should get an actor's data", async function () {
-      const { supplyChainTracking, addr1, addr2 } = await loadFixture(deploySupplyChainTrackingFixture);
-
-      const actor = addr1.address;
-      const role = 1;
-
-      await supplyChainTracking.registerActor(actor, role);
-
-      const [returnedRole, returnedStatus] = await supplyChainTracking.getActor(actor);
-
-      expect(returnedRole.toString()).to.equal("1");
-      expect(returnedStatus).to.equal(true);
-    })
-
-    it("Should not allow unauthorized address get an actor's data", async function () {
-      const { supplyChainTracking, addr1, addr2 } = await loadFixture(deploySupplyChainTrackingFixture);
-
-      const actor = addr1.address;
-      const role = 1;
-
-      await supplyChainTracking.registerActor(actor, role);
-
-      const actorAdata = supplyChainTracking.connect(addr2).getActor(actor);
-      await expect(actorAdata).to.be.revertedWithCustomError(supplyChainTracking, "OwnableUnauthorizedAccount");
-    })
-  });
-
   describe("Test Product functions", async function () {
     it("Should allow a Producer to register a new asset", async function () {
       const { supplyChainTracking, addr1 } = await loadFixture(deploySupplyChainTrackingFixture);
@@ -151,9 +84,10 @@ describe("SupplyChainTracking tests", function () {
     it("Should revert data retrieval if asset list is empty", async function () {
       const { supplyChainTracking, addr1 } = await loadFixture(deploySupplyChainTrackingFixture);
       await supplyChainTracking.registerActor(addr1.address, 0);
-
+      
       const result = supplyChainTracking.getAsset(1);
       await expect(result).to.be.revertedWith("Asset ID must be within valid range")
     })
+    
   });
 });
